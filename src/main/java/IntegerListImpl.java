24ed855f61fb04670a960dc9c14b9bd14e925e94
dmitryspeliy <dmitriypospelov93@gmail.com>
2022-10-17 01:33:28 +0700

    HWInteger

--- src/main/java/IntegerListImpl.java
+++ src/main/java/IntegerListImpl.java
@@ -2,28 +2,28 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.stream.IntStream;
 
-public class StringListImpl implements StringList {
-    private String[] arr;
+public class IntegerListImpl implements IntegerList {
+    private Integer[] arr;
 
-    public StringListImpl() {
-        arr = new String[0];
+    public IntegerListImpl() {
+        arr = new Integer[0];
     }
 
-    public StringListImpl(String... str) {
-        arr = new String[str.length];
+    public IntegerListImpl(Integer... str) {
+        arr = new Integer[str.length];
         System.arraycopy(str, 0, arr, 0, str.length);
     }
 
 
     @Override
-    public String add(String item) {
+    public Integer add(Integer item) {
         if (item == null) throw new NullPointerException("Вставить Null нельзя");
         if (arr.length == 0) {
-            arr = new String[1];
+            arr = new Integer[1];
             arr[0] = item;
             return item;
         }
-        String[] tmpArr = new String[arr.length + 1];
+        Integer[] tmpArr = new Integer[arr.length + 1];
         System.arraycopy(arr, 0, tmpArr, 0, arr.length);
         arr = tmpArr;
         arr[arr.length - 1] = item;
@@ -31,15 +31,15 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String add(int index, String item) {
+    public Integer add(int index, Integer item) {
         if (item == null) throw new NullPointerException("Вставить Null нельзя");
         if (index > arr.length || index < 0) throw new RuntimeException("Нет такого индекса");
         if (arr.length == 0) {
-            arr = new String[1];
+            arr = new Integer[1];
             arr[0] = item;
             return item;
         }
-        String[] tmpArr = new String[arr.length + 1];
+        Integer[] tmpArr = new Integer[arr.length + 1];
         System.arraycopy(arr, index, tmpArr, index + 1, arr.length - index);
         System.arraycopy(arr, 0, tmpArr, 0, index);
         tmpArr[index] = item;
@@ -48,11 +48,11 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String set(int index, String item) {
+    public Integer set(int index, Integer item) {
         if (item == null) throw new NullPointerException("Установить Null нельзя");
         if (index > arr.length || index < 0) throw new RuntimeException("Нет такого индекса");
         if (arr.length == 0) {
-            arr = new String[1];
+            arr = new Integer[1];
             arr[0] = item;
             return item;
         }
@@ -61,7 +61,7 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String remove(String item) {
+    public Integer remove(Integer item) {
         if (item == null) throw new NullPointerException("Удалить Null нельзя");
         int index = 0;
         boolean find = false;
@@ -72,37 +72,60 @@ public class StringListImpl implements StringList {
             }
         }
         if (!find) throw new NullPointerException("Такого элемента нет");
-        return getString(index, item);
+        return arr[index];
     }
 
     @Override
-    public String remove(int index) {
+    public Integer remove(int index) {
         if (isEmpty() || index < 0 || index >= arr.length) throw new NullPointerException("Нет такого индекса");
-        String elem = arr[index];
-        return getString(index, elem);
+        Integer elem = arr[index];
+        return getInteger(index, elem);
     }
 
-    private String getString(int index, String elem) {
-        String[] tmpArr = new String[arr.length - 1];
+    private Integer getInteger(int index, Integer elem) {
+        Integer[] tmpArr = new Integer[arr.length - 1];
         System.arraycopy(arr, index + 1, tmpArr, index, arr.length - index - 1);
         System.arraycopy(arr, 0, tmpArr, 0, index);
         arr = tmpArr;
         return elem;
     }
 
+
     @Override
-    public boolean contains(String item) {
+    public boolean contains(Integer item) {
         return Arrays.asList(arr).contains(item);
     }
 
+
+    public boolean BinarySearchContains(Integer item) {
+        sortingArr();
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (Objects.equals(item, arr[mid])) {
+                return true;
+            }
+
+            if (item < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+
     @Override
-    public int indexOf(String item) {
+    public int indexOf(Integer item) {
         if (item == null) throw new NullPointerException();
         return IntStream.range(0, arr.length).filter(i -> arr[i].equals(item)).findFirst().orElse(-1);
     }
 
     @Override
-    public int lastIndexOf(String item) {
+    public int lastIndexOf(Integer item) {
         if (item == null) throw new NullPointerException();
         int count = 0;
         for (int i = arr.length - 1; i >= 0; i--) {
@@ -115,13 +138,13 @@ public class StringListImpl implements StringList {
     }
 
     @Override
-    public String get(int index) {
+    public Integer get(int index) {
         if (index < 0 || index >= arr.length) throw new NullPointerException("Нет такого индекса");
         return arr[index];
     }
 
     @Override
-    public boolean equals(StringListImpl otherList) {
+    public boolean equals(IntegerListImpl otherList) {
         if (otherList == null) throw new NullPointerException();
         return Arrays.equals(otherList.getArr(), this.getArr());
     }
@@ -136,17 +159,29 @@ public class StringListImpl implements StringList {
         return arr.length == 0;
     }
 
-    @Override
-    public void clear() {
-        arr = new String[0];
+    private void sortingArr() {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
     }
 
     @Override
-    public String[] toArray() {
+    public void clear() {
+        arr = new Integer[0];
+    }
+
+    @Override
+    public Integer[] toArray() {
         return getArr();
     }
 
-    public String[] getArr() {
+    public Integer[] getArr() {
         return arr;
     }
 
--- src/test/java/StringListImplTest.java
+++ src/test/java/StringListImplTest.java
@@ -5,107 +5,107 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class StringListImplTest {
+class IntegerListImplTest {
 
-    private final StringListImpl stringList = new StringListImpl();
+    private final IntegerListImpl stringList = new IntegerListImpl();
 
     @BeforeEach
     public void init() {
-        stringList.add("1");
-        stringList.add("2");
-        stringList.add("3");
-        stringList.add("4");
-        stringList.add("5");
+        stringList.add(1);
+        stringList.add(2);
+        stringList.add(3);
+        stringList.add(4);
+        stringList.add(5);
     }
 
     @Test
     void add() {
-        assertTrue(stringList.contains("1"));
+        assertTrue(stringList.contains(1));
         assertFalse(stringList.contains(null));
-        assertEquals(stringList.indexOf("1"), 0);
+        assertEquals(stringList.indexOf(1), 0);
         assertEquals(stringList.size(), 5);
-        assertEquals(stringList.add("6"), "6");
+        assertEquals(stringList.add(6), 6);
         assertThrows(NullPointerException.class, () -> stringList.add(null));
     }
 
 
     @Test
     void testAdd() {
-        stringList.add(1, "test");
-        assertEquals(stringList.indexOf("test"), 1);
-        assertEquals(stringList.indexOf("2"), 2);
+        stringList.add(1, 121);
+        assertEquals(stringList.indexOf(121), 1);
+        assertEquals(stringList.indexOf(2), 2);
         assertFalse(stringList.contains(null));
         assertThrows(NullPointerException.class, () -> stringList.add(null));
-        assertThrows(RuntimeException.class, () -> stringList.add(1203, "test3"));
-        assertThrows(RuntimeException.class, () -> stringList.add(-12, "test3"));
+        assertThrows(RuntimeException.class, () -> stringList.add(1203, 34));
+        assertThrows(RuntimeException.class, () -> stringList.add(-12, 34));
 
     }
 
     @Test
     void set() {
-        stringList.set(0, "test");
-        assertFalse(stringList.contains("1"));
-        assertTrue(stringList.contains("test"));
+        stringList.set(0, 14);
+        assertFalse(stringList.contains(1));
+        assertTrue(stringList.contains(14));
         assertFalse(stringList.contains(null));
-        assertEquals(stringList.indexOf("test"), 0);
-        assertThrows(RuntimeException.class, () -> stringList.set(1203, "test3"));
-        assertThrows(RuntimeException.class, () -> stringList.set(-12, "test3"));
+        assertEquals(stringList.indexOf(14), 0);
+        assertThrows(RuntimeException.class, () -> stringList.set(1203, 3123));
+        assertThrows(RuntimeException.class, () -> stringList.set(-12, 3213));
         assertThrows(NullPointerException.class, () -> stringList.set(1, null));
-        stringList.set(2, "check");
-        assertEquals(2, stringList.indexOf("check"));
-        assertEquals(stringList.add("test11"), "test11");
+        stringList.set(2, 44);
+        assertEquals(2, stringList.indexOf(44));
+        assertEquals(stringList.add(55), 55);
     }
 
     @Test
     void remove() {
-        assertEquals("1", stringList.remove("1"));
-        assertFalse(stringList.contains("1"));
+        assertEquals(2, stringList.remove(1));
+        assertFalse(stringList.contains(2));
         assertFalse(stringList.contains(null));
         assertThrows(NullPointerException.class, () -> stringList.remove(null));
-        assertThrows(NullPointerException.class, () -> stringList.remove("check"));
+        assertThrows(NullPointerException.class, () -> stringList.remove(1231));
     }
 
     @Test
     void testRemove() {
         assertThrows(NullPointerException.class, () -> stringList.remove(1000));
         assertThrows(NullPointerException.class, () -> stringList.remove(-1));
-        assertEquals("1", stringList.remove(0));
+        assertEquals(1, stringList.remove(0));
         assertFalse(stringList.contains(null));
-        assertFalse(stringList.contains("1"));
+        assertFalse(stringList.contains(1));
     }
 
     @Test
     void contains() {
-        assertFalse(stringList.contains("hi"));
-        assertTrue(stringList.contains("1"));
+        assertFalse(stringList.contains(1433));
+        assertTrue(stringList.contains(1));
     }
 
     @Test
     void indexOf() {
-        assertEquals(-1, stringList.indexOf("hi"));
-        assertEquals(0, stringList.indexOf("1"));
+        assertEquals(-1, stringList.indexOf(1433));
+        assertEquals(0, stringList.indexOf(1));
         assertThrows(NullPointerException.class, () -> stringList.indexOf(null));
     }
 
     @Test
     void lastIndexOf() {
-        assertEquals(-1, stringList.lastIndexOf("hi"));
-        assertEquals(4, stringList.lastIndexOf("1"));
+        assertEquals(-1, stringList.lastIndexOf(1433));
+        assertEquals(4, stringList.lastIndexOf(1));
         assertThrows(NullPointerException.class, () -> stringList.lastIndexOf(null));
     }
 
     @Test
     void get() {
-        assertEquals("1", stringList.get(0));
+        assertEquals(1, stringList.get(0));
         assertThrows(NullPointerException.class, () -> stringList.get(1000));
         assertThrows(NullPointerException.class, () -> stringList.get(-1));
     }
 
     @Test
     void testEquals() {
-        StringListImpl test1 = new StringListImpl("1", "2", "3", "4", "5");
+        IntegerListImpl test1 = new IntegerListImpl(1,2,3,4,5);
         assertTrue(Arrays.equals(stringList.toArray(), test1.getArr()));
-        StringListImpl test2 = new StringListImpl("1", "check", "3", "4", "5");
+        IntegerListImpl test2 = new IntegerListImpl(1,12,2,3,4,5);
         assertFalse(Arrays.equals(stringList.toArray(), test2.getArr()));
     }
 
@@ -124,6 +124,12 @@ class StringListImplTest {
     void clear() {
         stringList.clear();
         assertEquals(0, stringList.size());
+    }
+
+    @Test
+    void BinaryResearch() {
+        assertFalse(stringList.BinarySearchContains(1433));
+        assertTrue(stringList.BinarySearchContains(1));
     }
 
 }
